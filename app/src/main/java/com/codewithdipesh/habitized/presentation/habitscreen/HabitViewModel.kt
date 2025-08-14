4f2fb5b42eefe4e0caa280cdcc695db05c84e851
Dipesh Pal <codewithdipesh2022@gmail.com>
2025-08-15 00:03:39 +0530

    add shareCard UI and functions

--- app/src/main/java/com/codewithdipesh/habitized/presentation/habitscreen/HabitViewModel.kt
+++ app/src/main/java/com/codewithdipesh/habitized/presentation/habitscreen/HabitViewModel.kt
@@ -1,23 +1,35 @@
 package com.codewithdipesh.habitized.presentation.habitscreen
 
 import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.codewithdipesh.habitized.domain.alarmManager.AlarmItem
 import com.codewithdipesh.habitized.domain.alarmManager.AlarmScheduler
 import com.codewithdipesh.habitized.domain.model.Frequency
+import com.codewithdipesh.habitized.domain.model.HabitProgress
 import com.codewithdipesh.habitized.domain.model.HabitType
 import com.codewithdipesh.habitized.domain.model.ImageProgress
 import com.codewithdipesh.habitized.domain.repository.HabitRepository
 import com.codewithdipesh.habitized.domain.util.getNextAlarmDateTime
 import com.codewithdipesh.habitized.presentation.util.IntToWeekDayMap
 import com.codewithdipesh.habitized.presentation.util.WeekDayMapToInt
+import dagger.hilt.android.internal.Contexts
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.UUID
@@ -31,6 +43,9 @@ class HabitViewModel @Inject constructor(
 
     private val _state = MutableStateFlow(HabitDetailsUI())
     val state = _state.asStateFlow()
+
+    private val _shareCardState = MutableStateFlow(ShareCardUI())
+    val shareCardState = _shareCardState.asStateFlow()
 
     suspend fun init( id : UUID){
         val habit = repo.getHabitById(id)
@@ -78,6 +93,16 @@ class HabitViewModel @Inject constructor(
         _state.value = _state.value.copy(
             imageProgresses = imageProgresses
         )
+        initShareCard(
+            title = habit.title,
+            frequency = habit.frequency,
+            streak = habit.currentStreak,
+            colorKey = habit.colorKey,
+            progressList = completed,
+            startDate = habit.start_date,
+            daysOfWeek = habit.days_of_week,
+            daysOfMonth = habit.daysOfMonth ?: emptyList()
+        )
     }
 
     suspend fun saveImage(id : UUID?,image : String, date : LocalDate, description : String){
@@ -114,6 +139,107 @@ class HabitViewModel @Inject constructor(
             alarmScheduler.cancel(alarmItem)
         }
     }
+
+    fun initShareCard(
+        title: String,
+        streak:Int,
+        startDate : LocalDate,
+        frequency : Frequency,
+        colorKey: String,
+        daysOfWeek: List<Int>,
+        daysOfMonth: List<Int>,
+        progressList: List<HabitProgress>
+    ){
+        val (weeklyRange, OverallRange) = getWeeklyandOverAllDates()
+        _shareCardState.value = _shareCardState.value.copy(
+            title = title,
+            currentStreak = streak,
+            frequency = frequency,
+            colorKey = colorKey,
+            progressList = progressList,
+            startDate = startDate,
+            daysOfWeek = daysOfWeek,
+            daysOfMonth = daysOfMonth,
+            WeeklyDateRange = weeklyRange,
+            OverAllDateRange = OverallRange
+        )
+    }
+    fun getWeeklyandOverAllDates() : Pair< List<LocalDate>,List<LocalDate> > {
+        val startOfWeek = LocalDate.now().with(DayOfWeek.MONDAY)
+        val weeklyRange = (0..6).map { offset ->
+            startOfWeek.plusDays(offset.toLong())
+        }
+        val monthlyRange = (0..(119 + LocalDate.now().dayOfWeek.value) )
+            .map{ offset ->
+                LocalDate.now().minusDays(offset.toLong())
+            }
+            .reversed()
+
+        return weeklyRange to monthlyRange
+    }
+    fun setBackground(bg: Background){
+        _shareCardState.value = _shareCardState.value.copy(
+            selectedBackground = bg
+        )
+    }
+    fun setImage(image: Bitmap){
+        _shareCardState.value = _shareCardState.value.copy(
+            imageBgBitmap = image
+        )
+    }
+    fun shareCard(context: Context, bitmap: Bitmap){
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Check out my progress")
+        shareIntent.type = "image/png"
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        val uri = getImageUri(context, bitmap)
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+        context.startActivity(Intent.createChooser(shareIntent, "Share Progress"))
+    }
+    fun getImageUri(context: Context, bitmap: Bitmap): Uri {
+        val imagesFolder = File(context.cacheDir, "shared_images")
+        imagesFolder.mkdirs()
+        val timeMillis = System.currentTimeMillis()
+        val fileName = "Habit_Progress_${timeMillis}.png"
+        val file = File(imagesFolder, fileName)
+        FileOutputStream(file).use { outputStream ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            outputStream.flush()
+        }
+
+        return FileProvider.getUriForFile(
+            context,
+            "${context.packageName}.provider",
+            file
+        )
+    }
+    fun downloadCard(context: Context, bitmap: Bitmap): Boolean {
+        return try {
+            val fileName = "Habit_Progress_${System.currentTimeMillis()}.png"
+            val picturesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val file = File(picturesDir, fileName)
+
+            FileOutputStream(file).use { out ->
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+            }
+
+            // Notify gallery about new image
+            MediaScannerConnection.scanFile(
+                context,
+                arrayOf(file.absolutePath),
+                arrayOf("image/png"),
+                null
+            )
+
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+
+
 
     fun clearUi(){
         _state.value = HabitDetailsUI()
